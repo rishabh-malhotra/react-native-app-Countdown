04e68151316decfc14762a7d0b3478b6f3e7ad15
rishabh-malhotra <rishabhmalhotra110796@gmail.com>
2019-06-27 09:51:05 +0530

    added additional components and a file for fetching data

--- EventList.js
+++ EventList.js
@@ -1,19 +1,29 @@
 import React,{Component} from 'react';
 import {Text,FlatList,View} from 'react-native';
+import EventCard from './EventCard';
 
 class EventList extends Component{
     constructor(props){
         super(props)
+        this.state={events:[]}
+    }
+    componentDidMount(){
+        const events=require('./db.json').events.map(e=>({
+            ...e,
+            date:new Date(e.date)
+        }));
+        this.setState({events});
     }
 
     render(){
         return(
             <View>
             <FlatList
-                data={[{name:'a'},{name:'b'}]}
-                renderItem={({item})=><Text>{item.name}</Text>}
+                data={this.state.events}
+                renderItem={({item})=><EventCard event={item}/>}
+                keyExtractor={item=>item.id}
             />
-            <Text>New Event Compnent {this.props.event}</Text>
+            <Text> New Event Component {this.props.event}</Text>
             </View>
         )
     }
